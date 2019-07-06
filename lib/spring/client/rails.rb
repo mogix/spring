55a96386525b6f0f5c62684255c9f59db7cb512d
mogix <mogix.yphd@gmail.com>
2019-07-07 00:02:12 +0900

    Fix to execute Rake tasks via Spring with `rails` command

--- lib/spring/client/rails.rb
+++ lib/spring/client/rails.rb
@@ -3,30 +3,54 @@ require "set"
 module Spring
   module Client
     class Rails < Command
-      COMMANDS = Set.new %w(console runner generate destroy test)
+      COMMANDS_VIA_SPRING = Set.new %w[
+        console runner generate destroy test
+      ]
+
+      BYPASS_COMMANDS = Set.new %w[
+        credentials:edit
+        credentials:show
+        dbconsole
+        encrypted:edit
+        encrypted:show
+        new
+        secrets:edit
+        secrets:setup
+        secrets:show
+        server
+        version
+        -h -? --help
+        -v --version
+      ]
 
       ALIASES = {
-        "c" => "console",
-        "r" => "runner",
-        "g" => "generate",
-        "d" => "destroy",
-        "t" => "test"
+        "g"  => "generate",
+        "d"  => "destroy",
+        "c"  => "console",
+        "s"  => "server",
+        "db" => "dbconsole",
+        "r"  => "runner",
+        "t"  => "test"
       }
 
       def self.description
-        "Run a rails command. The following sub commands will use Spring: #{COMMANDS.to_a.join ', '}."
+        "Run a rails command. " \
+        "The following sub commands will use Spring: " \
+        "#{COMMANDS_VIA_SPRING.to_a.join ', '}."
       end
 
       def call
         command_name = ALIASES[args[1]] || args[1]
 
-        if COMMANDS.include?(command_name)
+        if COMMANDS_VIA_SPRING.include?(command_name)
           Run.call(["rails_#{command_name}", *args.drop(2)])
-        else
+        elsif BYPASS_COMMANDS.include?(command_name) || command_name.nil?
           require "spring/configuration"
           ARGV.shift
           load Dir.glob(Spring.application_root_path.join("{bin,script}/rails")).first
           exit
+        else
+          Run.call(["rake", *args.drop(1)])
         end
       end
     end
